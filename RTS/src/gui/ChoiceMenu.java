45c2df94fec5edcc3a820a094c62a457148dd641
Chibizo <matheo.olevier95@gmail.com>
2025-02-23 22:25:21 +0100

    v10

--- RTS/src/gui/ChoiceMenu.java
+++ RTS/src/gui/ChoiceMenu.java
@@ -1,5 +1,8 @@
 package gui;
 
+import javax.swing.border.EmptyBorder;
+
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
@@ -33,6 +36,8 @@ import engine.process.MobileInterface;
 public class ChoiceMenu extends JFrame{
 	
 	
+	private String race;
+	
 	private Container contentPane;
 
 	private JPanel racePanel ;
@@ -58,6 +63,13 @@ public class ChoiceMenu extends JFrame{
 	
 	public ChoiceMenu(String title) {
 		super(title);
+		init();
+		initStyle();
+		initAction();
+		this.setSize(800,400);
+	    this.setLocationRelativeTo(null);
+
+
 		
 	}
 	
@@ -66,7 +78,7 @@ public class ChoiceMenu extends JFrame{
 		contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 		
-		racePanel=new JPanel(new FlowLayout());
+		racePanel=new JPanel(new FlowLayout(FlowLayout.CENTER,50,10));
 		racePanel.add(humanButton);
 		racePanel.add(elfButton);
 		racePanel.add(dwarfButton);
@@ -81,18 +93,100 @@ public class ChoiceMenu extends JFrame{
 		contentPane.add(racePanel,BorderLayout.CENTER);
 		
 		
-		
-		
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    pack();
+	    setVisible(true);
+
 	}
 	
 	
 	public void initStyle() {
-		
+	    
+	    raceLabel.setFont(new Font("Nimbus Sans", Font.BOLD, 25));
+	    raceLabel.setForeground(new java.awt.Color(255, 255, 255));
+	    raceLabel.setHorizontalAlignment(JLabel.CENTER);
+	    raceLabel.setBorder(new EmptyBorder(100, 0, 40, 0));
+
+	    
+	    humanButton.setFont(new Font("Nimbus Sans", Font.BOLD, 25));
+	    humanButton.setBackground(new java.awt.Color(45, 45, 45));  
+	    humanButton.setForeground(new java.awt.Color(255, 255, 255));
+	    humanButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+	    
+	    elfButton.setFont(new Font("Nimbus Sans", Font.BOLD, 25));
+	    elfButton.setBackground(new java.awt.Color(45, 45, 45));  
+	    elfButton.setForeground(new java.awt.Color(255, 255, 255));
+	    elfButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+	    
+	    dwarfButton.setFont(new Font("Nimbus Sans", Font.BOLD, 25));
+	    dwarfButton.setBackground(new java.awt.Color(45, 45, 45));  
+	    dwarfButton.setForeground(new java.awt.Color(255, 255, 255));
+	    dwarfButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+	    
+	    
+	    
+	    getContentPane().setBackground(new Color(33, 33, 33));
+	    racePanel.setBackground(new Color(33, 33, 33));
+	    numberPlayerPanel.setBackground(new Color(33, 33, 33));
+
+
+
+
 	}
 	
 	public void initAction() {
-		
+		humanButton.addActionListener(new HumanAction(this));
+		elfButton.addActionListener(new ElfAction(this));
+		dwarfButton.addActionListener(new DwarfAction(this));
+
 	}
+	
+	
+	public class HumanAction implements ActionListener {
+			
+			private JFrame fenetre;
+			public HumanAction(JFrame fenetre) {
+				this.fenetre=fenetre;
+			}
+			
+			public void actionPerformed(ActionEvent e) {
+				race="human";
+				fenetre.dispose();	
+				
+			}
+		}
+	
+	
+	public class ElfAction implements ActionListener {
+		
+		private JFrame fenetre;
+		public ElfAction(JFrame fenetre) {
+			this.fenetre=fenetre;
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			race="human";
+			fenetre.dispose();	
+			
+		}
+	}
+	
+	public class DwarfAction implements ActionListener {
+		
+		private JFrame fenetre;
+		public DwarfAction(JFrame fenetre) {
+			this.fenetre=fenetre;
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			race="human";
+			fenetre.dispose();	
+			
+		}
+	}
+	
+	
+	
 }
 
 
--- RTS/src/launcher/Starter.java
+++ RTS/src/launcher/Starter.java
@@ -1,15 +1,20 @@
 package launcher;
 
+import gui.ChoiceMenu;
 import gui.MainGUI;
 
 public class Starter {
 
 	public static void main(String[] args) {
-
+		/**
+		 * 
+		 
 		MainGUI gameMainGUI = new MainGUI("RTS");
 
 		Thread gameThread = new Thread(gameMainGUI);
 		gameThread.start();
+		**/
+		new ChoiceMenu("Menu");
 	}
 
 }
